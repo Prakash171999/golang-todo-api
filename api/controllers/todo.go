dcb34f9e25bf3bfe094cb3050f7b5263b2c4ed4c
Prakash Rai <prakash.bahing21@gmail.com>
2022-07-28 14:30:36 +0545

    Feat: Todo controller

---
+++ api/controllers/todo.go
@@ -0,0 +1,49 @@
+package controllers
+
+import ()
+
+// TodoController -> struct
+type TodoController struct {
+    logger                 infrastructure.Logger
+    TodoService  services.TodoService
+}
+
+// NewTodoController -> constructor
+func NewTodoController(
+    logger infrastructure.Logger,
+    TodoService services.TodoService,
+) TodoController {
+    return TodoController{
+        logger:                  logger,
+        TodoService:  TodoService,
+    }
+}
+
+//CreateTodo -> Create single todo
+func (cc TodoController) CreateTodo(c *gin.Context){
+	todo := models.Todo{}
+
+	if err := c.ShouldBindJSON(&todo); err != nil{
+		cc.logger.Zap.Error("Error [CreateTodo] (ShouldBindJson) : ", err)
+		err := errors.BadRequest.Wrap(err, "Failed to create todo")
+		responses.HandleError(c, err)
+		return
+	}
+
+	responses.SuccessJSON(c, http.StatusOK, "Todo Created Successfully")
+}
+
+//GetAllTodo -> Get All Todo list
+func (cc TodoController) GetAllTodo(c *gin.Context){
+	pagination := utils.BuildPagination(c)
+	pagination.Sort = "created_at_desc"
+	todos, count, err := cc.TodoService.GetAllTodo(pagination)
+
+	if err != nil {
+		cc.logger.Zap.Error("Error finding Todo records", err.Error())
+        err := errors.InternalError.Wrap(err, "Failed To Find Todo")
+        responses.HandleError(c, err)
+        return
+	}
+	responses.JSONCount(c, http.StatusOK, todos, count)
+}
