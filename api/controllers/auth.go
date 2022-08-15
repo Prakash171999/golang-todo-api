a570db90ce76ce85f95c1e107f4e0973aa4f7219
Prakash Rai <prakash.bahing21@gmail.com>
2022-08-15 14:28:25 +0545

    Fix:Controller file for user register api

---
+++ api/controllers/auth.go
@@ -0,0 +1,42 @@
+package controllers
+
+import (
+	"boilerplate-api/api/responses"
+	"boilerplate-api/api/services"
+	"boilerplate-api/errors"
+	"boilerplate-api/infrastructure"
+	"boilerplate-api/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type UserAuthController struct {
+	logger          infrastructure.Logger
+	UserAuthService services.UserAuthService
+}
+
+func NewAuthUserController(logger infrastructure.Logger, UserAuthService services.UserAuthService) UserAuthController {
+	return UserAuthController{
+		logger:          logger,
+		UserAuthService: UserAuthService,
+	}
+}
+
+func (cc UserAuthController) CreateUser(c *gin.Context) {
+	user := models.User{}
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		err := errors.BadRequest.Wrap(err, "Failed to create user")
+		responses.HandleError(c, err)
+		return
+	}
+
+	_, err := cc.UserAuthService.CreateUser(user)
+
+	if err != nil {
+		responses.HandleError(c, err)
+	}
+
+	responses.SuccessJSON(c, http.StatusOK, gin.H{"status": "User created successfully", "user": user})
+}
